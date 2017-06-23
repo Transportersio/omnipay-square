6b5fa7fe2257fe41c855bfb22f7652648e9ba7d9
Siriphan Boonmee <m.basicman@msn.com>
2017-06-23 15:49:53 +0700

    add callback orders

--- src/Message/TransactionRequest.php
+++ src/Message/TransactionRequest.php
@@ -70,29 +70,29 @@ class TransactionRequest extends AbstractRequest
         $api_instance = new SquareConnect\Api\TransactionsApi();
 
         try {
-            $result = $api_instance->retrieveTransaction($this->getLocationId(),$data['transactionId']);
+            $result = $api_instance->retrieveTransaction($this->getLocationId(), $data['transactionId']);
 
             $orders = array();
 
             $lineItems = $result->getTransaction()->getOrder()->getLineItems();
             if (count($lineItems) > 0) {
-                foreach ($lineItems as $key => $value){
+                foreach ($lineItems as $key => $value) {
                     $data = array();
                     $data['name'] = $value->getName();
                     $data['quantity'] = $value->getQuantity();
                     $data['amount'] = $value->getTotalMoney()->getAmount();
                     $data['currency'] = $value->getTotalMoney()->getCurrency();
-                    array_push($orders,$data);
+                    array_push($orders, $data);
                 }
             }
 
-            if($error = $result->getErrors()){
+            if ($error = $result->getErrors()) {
                 $response = array(
                     'status' => 'error',
                     'code' => $error['code'],
                     'detail' => $error['detail']
                 );
-            }else{
+            } else {
                 $response = array(
                     'status' => 'success',
                     'transactionId' => $result->getTransaction()->getId(),
