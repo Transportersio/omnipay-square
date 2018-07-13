5cd590cf9dc1d484280bfef1a4be8a25abded9a4
Mike Beck <mike@mikeybeck.com>
2018-07-13 17:52:09 +1200

    Revert TransactionRequest file

--- src/Message/TransactionRequest.php
+++ src/Message/TransactionRequest.php
@@ -51,19 +51,10 @@ class TransactionRequest extends AbstractRequest
         return $this->setParameter('transactionId', $value);
     }
 
-    public function getIdempotencyKey()
-    {
-        return $this->getParameter('idempotencyKey');
-    }
-
-    public function setIdempotencyKey($value)
-    {
-        return $this->setParameter('idempotencyKey', $value);
-    }
 
     public function getData()
     {
-        $data = [];
+        $data = array();
 
         $data['checkoutId'] = $this->getCheckoutId();
         $data['transactionId'] = $this->getTransactionId();
@@ -78,45 +69,40 @@ class TransactionRequest extends AbstractRequest
 
         $api_instance = new SquareConnect\Api\TransactionsApi();
 
-        $result = $api_instance->charge($this->getLocationId(), $data);
-        
+        try {
+            $result = $api_instance->retrieveTransaction($this->getLocationId(), $data['transactionId']);
 
-        return $this->createResponse($response);
+            $orders = array();
 
-//        try {
-//            $result = $api_instance->retrieveTransaction($this->getLocationId(), $data['transactionId']);
-//
-//            $orders = array();
-//
-//            $lineItems = $result->getTransaction()->getTenders();
-//            if (count($lineItems) > 0) {
-//                foreach ($lineItems as $key => $value) {
-//                    $data = array();
-//                    $data['quantity'] = 1;
-//                    $data['amount'] = $value->getAmountMoney()->getAmount()/100;
-//                    $data['currency'] = $value->getAmountMoney()->getCurrency();
-//                    array_push($orders, $data);
-//                }
-//            }
-//
-//            if ($error = $result->getErrors()) {
-//                $response = array(
-//                    'status' => 'error',
-//                    'code' => $error['code'],
-//                    'detail' => $error['detail']
-//                );
-//            } else {
-//                $response = array(
-//                    'status' => 'success',
-//                    'transactionId' => $result->getTransaction()->getId(),
-//                    'referenceId' => $result->getTransaction()->getReferenceId(),
-//                    'orders' => $orders
-//                );
-//            }
-//            return $this->createResponse($response);
-//        } catch (Exception $e) {
-//            echo 'Exception when calling LocationsApi->listLocations: ', $e->getMessage(), PHP_EOL;
-//        }
+            $lineItems = $result->getTransaction()->getTenders();
+            if (count($lineItems) > 0) {
+                foreach ($lineItems as $key => $value) {
+                    $data = array();
+                    $data['quantity'] = 1;
+                    $data['amount'] = $value->getAmountMoney()->getAmount()/100;
+                    $data['currency'] = $value->getAmountMoney()->getCurrency();
+                    array_push($orders, $data);
+                }
+            }
+
+            if ($error = $result->getErrors()) {
+                $response = array(
+                    'status' => 'error',
+                    'code' => $error['code'],
+                    'detail' => $error['detail']
+                );
+            } else {
+                $response = array(
+                    'status' => 'success',
+                    'transactionId' => $result->getTransaction()->getId(),
+                    'referenceId' => $result->getTransaction()->getReferenceId(),
+                    'orders' => $orders
+                );
+            }
+            return $this->createResponse($response);
+        } catch (Exception $e) {
+            echo 'Exception when calling LocationsApi->listLocations: ', $e->getMessage(), PHP_EOL;
+        }
     }
 
     public function createResponse($response)
