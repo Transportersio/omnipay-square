91cf72376cf3bb485786cc6127083ea72f429ef2
Danijel Predojevic <danijel.predojevic@am2studio.hr>
2018-08-08 17:40:37 +0200

    feat(charge): Better handle the exceptions

--- src/Message/ChargeRequest.php
+++ src/Message/ChargeRequest.php
@@ -11,126 +11,135 @@ use SquareConnect;
 class ChargeRequest extends AbstractRequest
 {
 
-    public function getAccessToken()
-    {
-        return $this->getParameter('accessToken');
-    }
+	public function getAccessToken()
+	{
+		return $this->getParameter('accessToken');
+	}
 
-    public function setAccessToken($value)
-    {
-        return $this->setParameter('accessToken', $value);
-    }
+	public function setAccessToken($value)
+	{
+		return $this->setParameter('accessToken', $value);
+	}
 
-    public function getLocationId()
-    {
-        return $this->getParameter('locationId');
-    }
+	public function getLocationId()
+	{
+		return $this->getParameter('locationId');
+	}
 
-    public function setLocationId($value)
-    {
-        return $this->setParameter('locationId', $value);
-    }
+	public function setLocationId($value)
+	{
+		return $this->setParameter('locationId', $value);
+	}
 
-    public function getCheckoutId()
-    {
-        return $this->getParameter('checkoutId');
-    }
+	public function getCheckoutId()
+	{
+		return $this->getParameter('checkoutId');
+	}
 
-    public function setCheckoutId($value)
-    {
-        return $this->setParameter('ReceiptId', $value);
-    }
+	public function setCheckoutId($value)
+	{
+		return $this->setParameter('ReceiptId', $value);
+	}
 
-    public function getTransactionId()
-    {
-        return $this->getParameter('transactionId');
-    }
+	public function getTransactionId()
+	{
+		return $this->getParameter('transactionId');
+	}
 
-    public function setTransactionId($value)
-    {
-        return $this->setParameter('transactionId', $value);
-    }
+	public function setTransactionId($value)
+	{
+		return $this->setParameter('transactionId', $value);
+	}
 
-    public function setCardNonce($value) {
-        return $this->setParameter('card_nonce', $value);
-    }
+	public function setCardNonce($value) {
+		return $this->setParameter('card_nonce', $value);
+	}
 
 
-    public function getData()
-    {
-        $data = array(
-            'idempotency_key' => uniqid(),
-            'amount_money' => new SquareConnect\Model\Money(array(
-                'amount' => intval($this->getParameter('amount')*100),
-                'currency' => $this->getParameter('currency')
-            )),
-            'card_nonce' => $this->getParameter('card_nonce'),
-        );
+	public function getData()
+	{
+		$data = array(
+			'idempotency_key' => uniqid(),
+			'amount_money' => new SquareConnect\Model\Money(array(
+				'amount' => intval($this->getParameter('amount')*100),
+				'currency' => $this->getParameter('currency')
+			)),
+			'card_nonce' => $this->getParameter('card_nonce'),
+		);
 
-        return $data;
-    }
+		return $data;
+	}
 
-    public function sendData($data)
-    {
+	public function sendData($data)
+	{
 
-        SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($this->getAccessToken());
+		SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($this->getAccessToken());
 
-        $api_instance = new SquareConnect\Api\TransactionsApi();
+		$api_instance = new SquareConnect\Api\TransactionsApi();
 
-        try {
-            $result = $api_instance->charge($this->getLocationId(), $data);
+		try {
+			$result = $api_instance->charge($this->getLocationId(), $data);
 
-            $orders = array();
+			$orders = array();
 
-            $lineItems = $result->getTransaction()->getTenders();
-            if (count($lineItems) > 0) {
-                foreach ($lineItems as $key => $value) {
-                    $data = array();
-                    $data['quantity'] = 1;
-                    $data['amount'] = $value->getAmountMoney()->getAmount()/100;
-                    $data['currency'] = $value->getAmountMoney()->getCurrency();
+			$lineItems = $result->getTransaction()->getTenders();
+			if (count($lineItems) > 0) {
+				foreach ($lineItems as $key => $value) {
+					$data = array();
+					$data['quantity'] = 1;
+					$data['amount'] = $value->getAmountMoney()->getAmount()/100;
+					$data['currency'] = $value->getAmountMoney()->getCurrency();
 
-                    if($value->getType() == 'CARD') {
-                        $data['card_details'] = $value->getCardDetails();
-                    }
+					if($value->getType() == 'CARD') {
+						$data['card_details'] = $value->getCardDetails();
+					}
 
-                    array_push($orders, $data);
-                }
-            }
+					array_push($orders, $data);
+				}
+			}
 
-            if ($error = $result->getErrors()) {
-                $response = array(
-                    'status' => 'error',
-                    'code' => $error['code'],
-                    'detail' => $error['detail']
-                );
-            } else {
-                $response = array(
-                    'status' => 'success',
-                    'transactionId' => $result->getTransaction()->getId(),
-                    'transaction' => $result->getTransaction()->getTenders()[0],
-                    'referenceId' => $result->getTransaction()->getReferenceId(),
-                    'orders' => $orders
-                );
-            }
-            return $this->createResponse($response);
-        } catch (\Exception $e) {
-	        $response = array(
-		        'status' => false,
-		        'message' => $e->getMessage(),
-	        );
+			if ($error = $result->getErrors()) {
+				$response = array(
+					'status' => 'error',
+					'code' => $error['code'],
+					'detail' => $error['detail']
+				);
+			} else {
+				$response = array(
+					'status' => 'success',
+					'transactionId' => $result->getTransaction()->getId(),
+					'transaction' => $result->getTransaction()->getTenders()[0],
+					'referenceId' => $result->getTransaction()->getReferenceId(),
+					'orders' => $orders
+				);
+			}
+			return $this->createResponse($response);
+		} catch (SquareConnect\ApiException $e) {
+			$response = array(
+				'status' => false,
+				'message' => '',
+			);
 
-        	return $this->createResponse($response);
-        }
-    }
+			foreach ( $e->getResponseBody()->errors as $error ) {
+				$response['message'] .= $error->detail . PHP_EOL;
+			}
+
+			return $this->createResponse($response);
+		} catch(\Exception $e) {
+			return array(
+				'status' => false,
+				'message' => 'Something went wrong when trying to charge you card. Please try again.',
+			);
+		}
+	}
 
 	/**
 	 * @param $response
 	 *
 	 * @return TransactionResponse
 	 */
-    public function createResponse($response)
-    {
-        return $this->response = new TransactionResponse($this, $response);
-    }
+	public function createResponse($response)
+	{
+		return $this->response = new TransactionResponse($this, $response);
+	}
 }
