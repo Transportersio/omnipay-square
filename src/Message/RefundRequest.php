df1ae0c6516503519f29c94a1931484fac3ad8e4
Aleksandar Milic <aleksandar.milic@am2studio.hr>
2019-11-06 10:15:22 +0100

    fix(RefundRequest): Parse amount value to integer

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -83,8 +83,8 @@ class RefundRequest extends AbstractRequest
             'tender_id' => $transactionResponse->getTransaction()->getTenders()[0]->getId(),
             'reason'    => $this->getReason(),
             'amount_money' => new SquareConnect\Model\Money(array(
-                'amount' => $this->getParameter('amount'),
-                'currency' => $this->getParameter('currency')
+                'amount'    => (int) $this->getParameter('amount'),
+                'currency'  => $this->getParameter('currency')
             )),
         );
 
